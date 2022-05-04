d924d292ce4845688a9c8d97d4bcbd438381866e
Ioana Ailenei <65042817+aileneiioana@users.noreply.github.com>
2022-05-04 19:38:10 +0300

    Added Login Functionality

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,7 @@ namespace HotelManagementApplicationlication
 {
     public partial class Form1 : Form
     {
+        Staff_tbl model = new Staff_tbl();
         public Form1()
         {
             InitializeComponent();
@@ -20,9 +21,21 @@ namespace HotelManagementApplicationlication
 
         private void button1_Click(object sender, EventArgs e)
         {
+           
+            using (HoteldbEntities db = new HoteldbEntities())
+            {
+                var dataset = db.Staff_tbl.Where(x => x.Staffname == usernametb.Text && x.Staffpassword == passwordtb.Text).Select(x => new { x.Staffname, x.Staffpassword }).FirstOrDefault();
+                if(dataset != null)
+                {
             Main mf = new Main();
             mf.Show();
             this.Hide();
+
+                }
+                else MessageBox.Show("Wrong Username or Password!");
+
+            }
+
         }
     }
 }
