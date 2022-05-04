51f8cd72a3980b18285c20640bdc869c1e87f5c0
Ioana Ailenei <65042817+aileneiioana@users.noreply.github.com>
2022-05-04 19:29:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace HotelManagementApplicationlication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ReservationInfo());
+            Application.Run(new Form1());
         }
     }
 }
