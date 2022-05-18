7d33fd0e7f12bc789004198941d74ccc5f6eecd5
Ioana Ailenei <65042817+aileneiioana@users.noreply.github.com>
2022-05-18 21:08:36 +0300

    Added UnitTest solution

---
+++ IStaff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagementApplication
+{
+    public interface IStaff
+    {
+        int StaffId { get; set; }
+
+        string Staffname { get; set; }
+
+        string Staffpassword { get; set; }
+
+        string Gender { get; set; }
+
+
+    }
+}
