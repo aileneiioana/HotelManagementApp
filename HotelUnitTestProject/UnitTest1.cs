d4511527779be7264da3788e69e67e4236078609
Ioana Ailenei <65042817+aileneiioana@users.noreply.github.com>
2022-05-18 21:00:23 +0300

    Added UnitTesting

---
+++ HotelUnitTestProject/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using HotelManagementApplication;
+using System;
+
+namespace HotelUnitTestProject
+{
+    [TestClass]
+    public class DBUnitTest
+    {
+        [TestMethod]
+        public void DBStaffNameTestMethod()
+        {
+            var mockStaff = new Mock<Staff_tbl>();    
+            var staff = new Mock<IStaff>();
+            staff.Setup(p => p.StaffId).Returns(1);
+            staff.SetupGet(p => p.Staffname).Returns("ioana");
+            Assert.AreEqual("ioana", staff.Object.Staffname);
+
+        }
+
+        [TestMethod]
+        public void DBStaffPasswordTestMethod()
+        {
+            var mockStaff = new Mock<Staff_tbl>();
+            var staff = new Mock<IStaff>();
+            staff.SetupGet(p => p.StaffId).Returns(1);
+            staff.SetupGet(p => p.Staffpassword).Returns("1211");
+            Assert.AreEqual("1211", staff.Object.Staffpassword);
+
+        }
+    }
+}
